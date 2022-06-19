bed82f8a1a6c24a500426a6d2a24bc957847d09a
DmitryKisel85 <dim03@mail.ru>
2022-06-19 21:30:08 +0300

    Add FormInput component and its styles Refactir SignUpForm with FormInput component

---
+++ src/component/formInput/FormInput.js
@@ -0,0 +1,12 @@
+import "./formInput.scss";
+
+const FormInput = ({ label, ...otherProps }) => {
+	return (
+		<div className='group'>
+			<input className='form-input' {...otherProps} />
+			{label && <label className={`${otherProps.value.length > 0 ? "shrink" : ""} form-input-label`}>{label}</label>}
+		</div>
+	);
+};
+
+export default FormInput;
