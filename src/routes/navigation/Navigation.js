8096827dae7debeebba6346fdde55b5f40a13962
DmitryKisel85 <dim03@mail.ru>
2022-06-21 20:38:43 +0300

    Add CartDropdown and CartIcon component and their stylesheets

--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../component/cartIcon";
+import CartDropdown from "../../component/cartDropdown";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../context/user.context";
 
@@ -30,7 +33,9 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				<CartDropdown />
 			</div>
 			<Outlet />
 		</>
