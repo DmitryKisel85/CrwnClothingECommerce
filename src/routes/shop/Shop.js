103bf2b16b9d36b6be16685545c3193cb81f65d3
DmitryKisel85 <dim03@mail.ru>
2022-06-23 21:49:16 +0300

    Add CategoriesPreview component and refactor routes in Shop component

--- src/routes/shop/Shop.js
+++ src/routes/shop/Shop.js
@@ -1,21 +1,14 @@
-import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { CategoriesContext } from "../../context/categories.context";
-
-import CategoryPreview from "../../component/categoryPreview";
+import CategoriesPreview from "../categoriesPreview";
 
 import "./shop.scss";
 
 const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
-
 	return (
-		<div className='shop-container'>
-			{Object.keys(categoriesMap).map((title) => {
-				const products = categoriesMap[title];
-				return <CategoryPreview key={title} title={title} products={products} />;
-			})}
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+		</Routes>
 	);
 };
 
