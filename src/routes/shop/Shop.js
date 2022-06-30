ad4d04c36c44d69ead31d3f14d58996dde06f2c8
DmitryKisel85 <dim03@mail.ru>
2022-06-30 17:48:03 +0300

    Finish refactoring Category reducer

--- src/routes/shop/Shop.js
+++ src/routes/shop/Shop.js
@@ -6,15 +6,15 @@ import CategoriesPreview from "../categoriesPreview";
 import Category from "../category";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 const Shop = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		const getCategoriesMap = async () => {
-			const categoryMap = await getCategoriesAndDocuments();
-			dispatch(setCategoriesMap(categoryMap));
+			const categoriesArray = await getCategoriesAndDocuments("categories");
+			dispatch(setCategories(categoriesArray));
 		};
 
 		getCategoriesMap();
