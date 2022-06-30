2a3d665ec8bf1b83a64be687607ff2aa22302a4b
DmitryKisel85 <dim03@mail.ru>
2022-06-30 18:24:42 +0300

    Add custom loggerMiddleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,24 @@
-// import { createStore } from "redux";
-// import { devToolsEnhancer } from "@redux-devtools/extension";
-
-// import { rootReducer } from "./rootReducer";
-
-// export const store = createStore(
-// 	rootReducer,
-// 	/* preloadedState, */ devToolsEnhancer()
-// 	// Specify name here, actionsDenylist, actionsCreators and other options if needed
-// );
-
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./rootReducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+const loggerMiddleware = (store) => (next) => (action) => {
+	if (!action.type) {
+		return next(action);
+	}
+
+	console.log("type: ", action.type);
+	console.log("payload: ", action.payload);
+	console.log("current state: ", store.getState());
+
+	next(action);
+
+	console.log("next state: ", store.getState());
+};
+
+// const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
