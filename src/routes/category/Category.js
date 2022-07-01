73575c9d854facd221a274b13619d1c723410840
DmitryKisel85 <dim03@mail.ru>
2022-07-01 22:50:47 +0300

    Add Spinner to show while fetching categories from server

--- src/routes/category/Category.js
+++ src/routes/category/Category.js
@@ -3,14 +3,16 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import ProductCard from "../../component/productCard";
+import Spinner from "../../component/spinner";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/category.selector";
 
 import { CategoryContainer, CategoryTitle } from "./category.styles";
 
 const Category = () => {
 	const { category } = useParams();
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading);
 
 	const [products, setProducts] = useState(categoriesMap[category]);
 
@@ -21,7 +23,7 @@ const Category = () => {
 	return (
 		<>
 			<CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-			<CategoryContainer>{products && products.map((product) => <ProductCard key={product.id} product={product} />)}</CategoryContainer>
+			{isLoading ? <Spinner /> : <CategoryContainer>{products && products.map((product) => <ProductCard key={product.id} product={product} />)}</CategoryContainer>}
 		</>
 	);
 };
