76747fb69a782ac2e095d7e04629bf596e0b9df1
DmitryKisel85 <dim03@mail.ru>
2022-07-05 19:00:33 +0300

    Finish stripe payment implementation

--- netlify/functions/createPaymentIntent.js
+++ netlify/functions/createPaymentIntent.js
@@ -4,9 +4,10 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
 	try {
 		const { amount } = JSON.parse(event.body);
+
 		const paymentIntent = await stripe.paymentIntents.create({
-			amount,
-			currency: "USD",
+			amount: amount,
+			currency: "usd",
 			payment_method_types: ["card"],
 		});
 
