c680306b60b3326f50247f52f9d2117b92c2ef8e
Dmitry Kisel <dim03@mail.ru>
2022-08-17 22:29:52 +0300

    Refactor app: delete unused files and code

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ import { store, persistor } from "./store/store";
 
 import { stripePromise } from "./utils/stripe/stripe";
 
-import reportWebVitals from "./reportWebVitals";
-
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
@@ -27,8 +25,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
